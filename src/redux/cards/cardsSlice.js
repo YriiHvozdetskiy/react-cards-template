d563368754b9d37718de4dabe56c479e00865ac7
Yrii Hvozdetskiy <hvozdetskiyyrii@gmail.com>
2022-11-26 16:40:59 +0200

    delete redux-persist

--- src/redux/cards/cardsSlice.js
+++ src/redux/cards/cardsSlice.js
@@ -1,5 +1,3 @@
-import {persistReducer} from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 import {createSlice} from '@reduxjs/toolkit';
 
 export const counterSlice = createSlice({
@@ -14,12 +12,5 @@ export const counterSlice = createSlice({
       },
    },
 });
-
-const counterConfig = {
-   key: 'counter',
-   storage,
-};
-
-export const counterReducer = persistReducer(counterConfig, counterSlice.reducer);
 
 export const {} = counterSlice.actions;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,28 +1,9 @@
 import {configureStore} from '@reduxjs/toolkit';
-
-import {
-   persistStore,
-   FLUSH,
-   REHYDRATE,
-   PAUSE,
-   PERSIST,
-   PURGE,
-   REGISTER,
-} from 'redux-persist';
-import {counterReducer} from './cards/cardsSlice';
+import {counterSlice} from './cards/cardsSlice';
 
 export const store = configureStore({
    reducer: {
-      counter:counterReducer
-
-   },
-   middleware (getDefaultMiddleware) {
-      return getDefaultMiddleware({
-         serializableCheck: {
-            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-         },
-      });
+      counter: counterSlice.reducer,
    },
 });
 
-export const persistor = persistStore(store);
