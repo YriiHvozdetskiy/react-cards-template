fe816fa3e4576ca0a04ef9d1e4370595570871cd
Yrii Hvozdetskiy <hvozdetskiyyrii@gmail.com>
2022-11-27 18:04:42 +0200

    make limitCards

--- src/components/CardsList/CardsList.js
+++ src/components/CardsList/CardsList.js
@@ -1,17 +1,25 @@
 import css from './CardsList.module.scss';
 import {Card} from '../Card/Card';
 import {useSelector} from 'react-redux';
-import {getCards} from '../../redux/cards/cardsSelectors';
-import {useEffect} from 'react';
+import {getCards, getCardsLength} from '../../redux/cards/cardsSelectors';
+import {useEffect, useState} from 'react';
 
 export const CardsList = () => {
+
    const cards = useSelector(getCards);
+   const [limit, setLimit] = useState(9);
+
+   const limitCards = cards.slice(0, limit);
+
+   const loadMore = () => {
+      setLimit(prevState => prevState + 9);
+   };
 
    return (
       <>
          {cards.length > 0 && <div className={css.wrapper}>
             <ul className={css.list}>
-               {cards.map(({id, title, text, link, src}) => (
+               {limitCards.map(({id, title, text, link, src}) => (
                   <Card
                      key={id}
                      title={title}
@@ -22,7 +30,10 @@ export const CardsList = () => {
                   />
                ))}
             </ul>
-            {cards.length > 9 && <button type={'button'} className={css.button}>Завантажити більше</button>}
+            {cards.length > limit && <button type={'button'}
+                                         className={css.button}
+                                         onClick={loadMore}>
+               Завантажити більше</button>}
          </div>}
       </>
    );
